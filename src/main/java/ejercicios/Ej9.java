72a660a1c11ec086900780723459b1c9999549a3
stringimad <imadelfahssi@gmail.com>
2021-10-21 12:29:27 +0200

    Crea clase llamada ej9 y añade metodo main

---
+++ src/main/java/ejercicios/Ej9.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicios;
+
+/**
+ *
+ * @author imad
+ */
+public class Ej9 {
+
+    public static void main(String[] args) {
+        int i = 0;
+        int j = 6;
+        while (i < 0) {
+            i++;
+            j++;
+        }
+        System.out.println(j);
+    }
+
+}

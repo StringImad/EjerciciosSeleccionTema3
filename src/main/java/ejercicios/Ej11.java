ab935a689948b4caafb31db44de2456ce7fc9a19
stringimad <imadelfahssi@gmail.com>
2021-10-21 12:38:01 +0200

    Crea clase ej11 añade metodo main y termina el bucle do while con el funcionamiento correcto del programa

---
+++ src/main/java/ejercicios/Ej11.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicios;
+
+/**
+ *
+ * @author imad
+ */
+public class Ej11 {
+    public static void main(String[] args) {
+        char letraMayus = 65;
+        
+        do{
+            System.out.print(letraMayus+" - ");
+                        letraMayus++;
+
+        }while(letraMayus!=91);
+    }
+}

72e6e498a3cced60a4526ebec337bd47c84bba85
stringimad <imadelfahssi@gmail.com>
2021-10-21 12:48:06 +0200

    Ejercicio terminado subida desde clase

---
+++ src/main/java/ejercicios/Ej12.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicios;
+
+/**
+ *
+ * @author imad
+ */
+public class Ej12 {
+
+    public static void main(String[] args) {
+       // char letrasMinus;
+
+        for (int i = 97; i < 122; i++) {
+         //   letrasMinus = (char) i;
+            System.out.print(i + " - ");
+        }
+    }
+
+}

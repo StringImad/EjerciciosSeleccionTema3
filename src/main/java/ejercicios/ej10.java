67ff26ac98759102c1ac752446c2b13aa277993d
stringimad <imadelfahssi@gmail.com>
2021-10-21 12:33:19 +0200

    Crea clas ej10 y añade el main con la logica del programa

---
+++ src/main/java/ejercicios/ej10.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicios;
+
+/**
+ *
+ * @author imad
+ */
+public class ej10 {
+    public static void main(String[] args) {
+        char letrasMinus;
+        
+        for (int i = 97; i < 122; i++) {
+            letrasMinus = (char) i;
+            System.out.print(letrasMinus+" - ");
+        }
+    }
+}

f5a9c07c1b21f022af86f30a6c2b3f0945d79929
stringimad <imadelfahssi@gmail.com>
2021-10-21 11:58:04 +0200

    añade el main del ej1

--- src/main/java/ejercicios/Ej1.java
+++ src/main/java/ejercicios/Ej1.java
@@ -1,3 +1,4 @@
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -9,5 +10,15 @@ package ejercicios;
  * @author imad
  */
 public class Ej1 {
-    
+
+    public static void main(String[] args) {
+        int x = 1;
+
+        while (x <= 100) {
+            System.out.print(x);
+            System.out.print(" - ");
+            x = x + 1;
+        }
+    }
+
 }
